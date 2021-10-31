63fb9c27e0182f072e4a9f5b37b4bf365619c6d8
luctst <lucas.tostee@gmail.com>
2021-10-31 13:52:30 +0100

    New module design with iife and update test for more simplicity

--- test/test.js
+++ test/test.js
@@ -1,65 +1,24 @@
+const request = require('supertest');
 const test = require('ava');
-const net = require('net');
+const express = require('express');
 
-const lib = require("../lib/index");
-const http = require('../lib/utils/http');
+const lib = require('../lib/index');
 
-test.before(async function (t) {
-    t.context.parser = new lib({
-        get: {
-            "api/u/:id": null,
-            "api/cards": {
-                name: {
-                    type: String,
-                },
-                children: {
-                    "/:id": {},
-                },
-            },
-        },
-        put: {},
-    });
-    const promise = new Promise(((resolve, reject) => {
-        const socket = new net.Socket();
+const app = express();
+/**
+ * Express logic
+ */
+app.use(express.json());
+app.use(lib({ get: true}))
 
-        const onError = () => {
-            socket.destroy();
-            reject();
-        };
+app.get('/', function (res, res) {
+    res.status(200).json({ status: 200 });
+})
 
-        socket.setTimeout(1000);
-        socket.once('error', onError);
-        socket.once('timeout', onError);
+test('Test', async function (t) {
+    const response = await request(app)
+    .get('/');
 
-        socket.connect(57084, 'localhost', () => {
-            socket.end();
-            resolve();
-        });
-    }));
-
-    try {
-        await promise;
-        return t.passed;
-    } catch (error) {
-        return t.fail('You should start express server before running tests');
-    }
-});
-
-test('First level keys must be http methods', function (t) {
-    t.context.parser.httpVerbs.forEach(function (method) {
-        if (!http.includes(method)) return t.fail();
-    });
-
-    t.pass();
-});
-
-test('First level keys must be object',  function (t) {
-    const httpMethods = Object.values(t.context.parser.routeConfig);
-
-    httpMethods.forEach(function (value) {
-        if (Array.isArray(value)) return t.fail();
-        if (typeof value !== 'object') return t.fail();
-    });
-
+    t.log(response.body);
     t.pass();
 });
