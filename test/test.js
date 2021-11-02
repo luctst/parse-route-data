6c668d16c416a4b02cd7be744ed74a9169dcff8b
luctst <lucas.tostee@gmail.com>
2021-11-02 21:32:29 +0100

    Write some test

--- test/test.js
+++ test/test.js
@@ -3,22 +3,31 @@ const test = require('ava');
 const express = require('express');
 
 const lib = require('../lib/index');
+const fakeData = require('./mock/fakeRouteConfig');
 
 const app = express();
 /**
  * Express logic
  */
 app.use(express.json());
-app.use(lib({ get: true}))
+// app.use(lib({ get: true}))
 
-app.get('/', function (res, res) {
+app.get('/', function (req, res) {
+    res.status(200).json({ status: 200 });
+})
+
+app.get('/:test', lib(fakeData), function (req, res) {
+    res.status(200).json({ status: 200 });
+})
+
+app.get("/api/:test/route/:secondTest", lib(fakeData), function (req, res) {
     res.status(200).json({ status: 200 });
 })
 
 test('Test', async function (t) {
     const response = await request(app)
-    .get('/');
+    .get("/api/paramsDynamic/route/secondParamsDynamic")
+    .query({ filter: '' })
 
-    t.log(response.body);
     t.pass();
 });
