a6d3b26d578e827009b49ef067a82b1f21e8629e
luctst <lucas.tostee@gmail.com>
2021-11-02 21:32:38 +0100

    Create mock data

---
+++ test/mock/fakeRouteConfig.js
@@ -0,0 +1,11 @@
+module.exports = {
+    get: {
+        '/:test': null,
+        "/api/:test/route/:secondTest": {
+            filter: {
+                type: String,
+                required: true,
+            }
+        }
+    },
+};
