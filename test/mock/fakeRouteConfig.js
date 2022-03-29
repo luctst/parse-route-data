45ab1ebd8f49cf01d019b43503e00909c80540bf
luctst <lucas.tostee@gmail.com>
2022-03-30 01:30:59 +0200

    Add utils fn to create routesManager

--- test/mock/fakeRouteConfig.js
+++ test/mock/fakeRouteConfig.js
@@ -1,77 +1,45 @@
-const { Types } = require('mongoose');
-
-module.exports = {
-  post: {
-    "/api/room/:uuid/account": {
-      theme: {
-        type: String,
-        required: true,
-        canBe: ['007bff', '6c757d', '28a745', '17a2b8', 'ffc107'],
-      },
-      roomId: {
-        type: String,
-        required: true,
-        match: /\b[0-9a-f]{8}\b-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-\b[0-9a-f]{12}\b/,
-      },
-    },
-    '/api/array/with/subschema': {
-      data: {
-        type: Array,
-        itemsType: Object,
-        itemsSchema: {
-          isCredit: {
-            type: Boolean,
-            required: true,
-          },
-          isFieldObjectId: {
-            type: Boolean,
-            required: true,
-          },
-          amount: {
-            type: Number,
-            required: true,
-            min: 0,
-          },
-          info: {
-            type: String,
-            maxlength: 75,
-          },
-          to: {
-            type: [Types.ObjectId, String],
-            required: true,
-          },
-          from: {
-            type: [Types.ObjectId, String],
-            required: true,
-          },
-        }
-      }
-    },
-  },
-  get: {
-    '/api/verify/objectid': {
-      oid: {
-        type: Types.ObjectId,
-        required: true,
-      }
-    },
-    '/api/room/:uuid': {
-      createSession: {
-        type: Boolean,
-        default: false,
-      }
-    },
-    '/:test': null,
-    "/api/:test/route/:secondTest": {
-      filter: {
-        type: String,
-        required: true,
-      },
-      test: {
-        type: Array,
-        itemsType: [String, Boolean],
-        required: true,
-      },
-    }
-  },
+const routes = {
+  post: require('../routes/post'),
+  get: require('../routes/get'),
 };
+
+function formatRoutesForConfig(routesMethodAsObject) {
+  const methodsAsKeys = Object.keys(routesMethodAsObject);
+
+  return methodsAsKeys.reduce(
+    (prev, current) => {
+      const oldPrev = [...prev];
+      const newRoutesData = routes[current].map((r) => ({ ...r, method: current }));
+
+      oldPrev.push(...newRoutesData);
+      return oldPrev;
+    },
+    [],
+  );
+};
+
+function generateRoutesConfig(routesConfig) {
+  const routesConfigFinally = routesConfig.reduce((prev, current) => {
+    if (!prev) {
+      return {
+        [current.method]: {
+          [current.path]: current.data,
+        },
+      };
+    }
+
+    const spreadPrev = { ...prev };
+    if (prev[current.method]) {
+      spreadPrev[current.method][current.path] = current.data;
+      return prev;
+    }
+
+    spreadPrev[current.method] = {};
+    spreadPrev[current.method][current.path] = current.data;
+    return spreadPrev;
+  }, null);
+
+  return routesConfigFinally;
+}
+
+module.exports = generateRoutesConfig(formatRoutesForConfig(routes));
